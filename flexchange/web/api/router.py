4700c3cab21bcd7f1723fd05b086d2d77735449f
SerSamgy <samgy7cool@gmail.com>
2023-04-21 15:20:31 +0000

    Include `monitoring` router

--- flexchange/web/api/router.py
+++ flexchange/web/api/router.py
@@ -1,8 +1,7 @@
 from fastapi.routing import APIRouter
 
-from flexchange.web.api import trades
+from flexchange.web.api import monitoring, trades
 
 api_router = APIRouter()
-# api_router.include_router(monitoring.router)
-# api_router.include_router(echo.router, prefix="/echo", tags=["echo"])
+api_router.include_router(monitoring.router)
 api_router.include_router(trades.router, prefix="/trades", tags=["trades"])
