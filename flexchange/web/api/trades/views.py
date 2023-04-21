0cbb5c0ff93d203be7da601b559e7fb3d2992527
SerSamgy <samgy7cool@gmail.com>
2023-04-21 15:16:20 +0000

    Clean docstrings for `trades` endpoints

--- flexchange/web/api/trades/views.py
+++ flexchange/web/api/trades/views.py
@@ -16,14 +16,7 @@ async def get_trades(
     delivery_day: date | None = None,
     trade_dao: TradeDAO = Depends(),
 ) -> list[TradeModel]:
-    """
-    Retrieve trade objects from the database.
-
-    :param trader_id: ID of trader, responsible for trade.
-    :param delivery_day: Day on which the traded energy has to be delivered.
-    :param trade_dao: DAO for trade models.
-    :return: list of trade objects from database.
-    """
+    """Retrieve trade objects from the database."""
     return await trade_dao.filter(trader_id=trader_id, delivery_day=delivery_day)
 
 
@@ -32,10 +25,5 @@ async def create_trade(
     new_trade_object: TradeModelDTO,
     trade_dao: TradeDAO = Depends(),
 ) -> None:
-    """
-    Creates trade model in the database.
-
-    :param new_trade_object: new trade model item.
-    :param trade_dao: DAO for trade models.
-    """
+    """Creates trade model in the database."""
     await trade_dao.create(**new_trade_object.dict())
