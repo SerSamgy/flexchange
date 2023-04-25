c3d810e91c259ab53d00196d971b95706a5d4243
SerSamgy <samgy7cool@gmail.com>
2023-04-25 15:59:33 +0000

    Make email field for User unique

--- flexchange/db/models/user.py
+++ flexchange/db/models/user.py
@@ -1,6 +1,6 @@
 from typing import TYPE_CHECKING
 
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.orm import Mapped, mapped_column, relationship  # pyright: ignore [reportGeneralTypeIssues]
 from sqlalchemy.sql.sqltypes import Boolean, Integer, String
 
 from flexchange.db.base import Base
@@ -16,7 +16,7 @@ class User(Base):
 
     id = mapped_column(Integer, primary_key=True, autoincrement=True)
     full_name = mapped_column(String(length=256), nullable=False)
-    email = mapped_column(String(length=255), index=True, nullable=False)
+    email = mapped_column(String(length=255), index=True, nullable=False, unique=True)
     hashed_password = mapped_column(String, nullable=False)
     is_superuser = mapped_column(Boolean, default=False, nullable=False)
 
